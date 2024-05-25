363ba841d22fb7d9cc532ed6b8e681fba0f4e6e7
S-V-I <ambitious.sv@gmail.com>
2024-05-25 17:57:49 +0300

    js-calc

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,58 @@
-import '../../script/test'
+// import '../../script/test'
 
-console.log('container')
+// console.log('container')
+
+// console.log(screenX)
+
+// setInterval(() => {
+//   if (scrollY > 100) {
+//     console.log(true)
+//   }
+// }, 1000)
+
+// let result = confirm('18+')
+// if (result === true) alert('Wellcome!')
+
+// prompt('Your age?', 5)
+// print()
+
+// const win = window.open(
+//   'http://localhost:3000/',
+//   'MyName',
+//   'left=100,top=100,width=620,height=320',
+// )
+
+// win.opener === win // false
+// win.opener === window // true
+
+// win.moveTo(200, 200)
+// win.moveBy(-100, -100)
+
+// win.resizeTo(100, 100)
+// win.resizeBy(-100, -100)
+
+// const data = { id: 43123123, name: "Ivan",}
+// data
+// {id: 43123123, name: 'Ivan'}
+
+// const jsonData = JSON.stringify(data)
+// jsonData
+// '{"id":43123123,"name":"Ivan"}'
+
+// localStorage.setItem('user', jsonData)
+// const result = localStorage.getItem('user');
+
+// result
+// '{"id":43123123,"name":"Ivan"}'
+
+// JSON.parse(result)
+// {id: 43123123, name: 'Ivan'}
+// id: 43123123
+// name: "Ivan"
+// [[Prototype]]: Object
+
+// localStorage.removeItem('user')
+// localStorage.getItem('user')
+// null
+
+// localStorage.clear()
